726c1006c3a00b8003739ef1a68c82b1c222c92a
Brian Pareja <parejabrianh12@gmail.com>
2024-03-11 20:35:33 -0500

    oeee

--- lib/infrastruture/datasources/api_order_datasource_imp.dart
+++ lib/infrastruture/datasources/api_order_datasource_imp.dart
@@ -1,9 +1,22 @@
 import 'package:colch_stat_app/domain/datasources/order_datasource.dart';
 import 'package:colch_stat_app/domain/entities/order.dart';
+import 'package:dio/dio.dart';
+import 'package:colch_stat_app/config/constants/enviroment.dart';
+
+
+
 
 class ApiOrderDataSourceImpl implements OrderDataSource {
+
+
+    //* Para hacer las peticiones
+  final _dio = Dio(BaseOptions(
+    baseUrl: Environment.apiUrl
+  ));
+  
   @override
   Future<List<Order>> getOrders() {
+
     // TODO: implement getOrders
     throw UnimplementedError();
   }
