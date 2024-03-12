667f79e25a869203485117489b3fe9c3984e1118
Brian Pareja <parejabrianh12@gmail.com>
2024-03-12 15:18:07 -0500

    oeee

--- lib/infrastruture/datasources/api_order_datasource_imp.dart
+++ lib/infrastruture/datasources/api_order_datasource_imp.dart
@@ -1,40 +1,34 @@
-import 'package:colch_stat_app/domain/datasources/order_datasource.dart';
-import 'package:colch_stat_app/domain/entities/order.dart';
+import 'package:colch_stat_app/presentation/providers/profile_provider.dart';
 import 'package:dio/dio.dart';
 import 'package:colch_stat_app/config/constants/enviroment.dart';
+import 'package:colch_stat_app/domain/datasources/order_datasource.dart';
+import 'package:colch_stat_app/domain/entities/order.dart';
 import 'package:colch_stat_app/infrastruture/models/sale_model.dart';
 
-
-
 class ApiOrderDataSourceImpl implements OrderDataSource {
+  //* Para hacer las peticiones
+  final Dio _dio;
 
-
-    //* Para hacer las peticiones
-  final _dio = Dio(BaseOptions(
-    baseUrl: Environment.apiUrl
-  ));
+  ApiOrderDataSourceImpl() : _dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
 
   @override
   Future<List<Order>> getOrders() async {
-
     try {
+      // Configura el interceptor para agregar el encabezado de autorización
+      
+      
       final response = await _dio.get('/ordenes');
+      
+      final data = response.data as List<dynamic>;
 
-      final orden = OrderModel.fromJson(response.data["ordenes"]).toOrderEntity();
+      final orden = OrderModel.convertOrdenesToOrdersList(data.cast<Map<String, dynamic>>());
 
-   
+      return orden;
+
     } catch (e) {
       // Capturar y relanzar la excepción
+      print(e);
       throw e;
-
     }
-    
-
-
-
-
-
-    throw UnimplementedError();
   }
-
 }
